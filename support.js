7fe28369255c57c200e2b330179581fcaeb3e19c
bsaldivaremc2 <bsaldivar.emc2@gmail.com>
2019-08-30 13:16:11 +0200

    2

--- support.js
+++ support.js
@@ -11,11 +11,12 @@ function create_result_row(parent_id,faces_right="",description="")
 {
   var row_html = `
   <div class="faces">
-    <div class="faces_top"></div>
-      <div class="face_left">
-        <canvas id="canvas_result_`+results_counter+`" ></canvas>
-      </div>
-      <div class="faces_right">`+faces_right+`</div>
+    <div class="faces_top">
+        <div class="face_left">
+          <canvas id="canvas_result_`+results_counter+`" ></canvas>
+        </div>
+        <div class="faces_right">`+faces_right+`</div>
+    </div>
     <div class="faces_bottom">`+description+`</div>
   </div>
   `;
